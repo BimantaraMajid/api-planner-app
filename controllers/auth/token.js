7eb6816735517a21680dd0c4e0d135fb4803663d
majid <bimantarasc@gmail.com>
2023-12-16 10:26:38 +0700

    update token expired with environment

--- controllers/auth/token.js
+++ controllers/auth/token.js
@@ -1,7 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (data = {}) => jwt.sign(data, process.env.JWT_KEY, { expiresIn: '1h' });
-const generateRefreshToken = (data = {}) => jwt.sign(data, process.env.JWT_KEY, { expiresIn: '7d' });
+const generateToken = (data = {}) => jwt.sign(data, process.env.JWT_KEY, {
+  expiresIn: process.env.TOKEN_EXPIRED,
+});
+const generateRefreshToken = (data = {}) => jwt.sign(data, process.env.JWT_KEY, {
+  expiresIn: process.env.REFRESH_TOKEN_EXPIRED,
+});
 
 module.exports = {
   generateToken,
