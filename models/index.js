7d4da259fab6bf18f2370f378646b24887c9e21d
majid <bimantarasc@gmail.com>
2023-12-21 07:08:05 +0700

    remove unuse

--- models/index.js
+++ models/index.js
@@ -12,21 +12,6 @@ db.activities = require('./activities')(sequelize, Sequelize.DataTypes);
 db.activities_tags = require('./activities_tags')(sequelize, Sequelize.DataTypes);
 db.tags = require('./tags')(sequelize, Sequelize.DataTypes);
 
-// const basename = path.basename(__filename);
-// fs
-//   .readdirSync(__dirname)
-//   .filter((file) => (
-//     file.indexOf('.') !== 0
-//       && file !== basename
-//       && file.slice(-3) === '.js'
-//       && file.indexOf('.test.js') === -1
-//   ))
-//   .forEach((file) => {
-//     // eslint-disable-next-line import/no-dynamic-require, global-require
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
