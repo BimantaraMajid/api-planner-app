c5fa13ce8fc3a7e8e74c5be1dd0cac26fa933fdd
majid <bimantarasc@gmail.com>
2023-12-25 04:15:35 +0700

    add dialectModule config sequelize

--- api/config/config.js
+++ api/config/config.js
@@ -5,5 +5,7 @@ module.exports = {
   database: process.env.DB_DATABASE_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
+  // eslint-disable-next-line global-require
+  dialectModule: require('pg'),
   logging: process.env.NODE_ENV !== 'production' ? console.info : false,
 };
