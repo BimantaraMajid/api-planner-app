ae124578318ee593ffbd8413107d225bbdd27d17
majid <bimantarasc@gmail.com>
2023-12-25 00:45:12 +0700

    use pagination payload check at router getAll

--- routes/activities/index.js
+++ routes/activities/index.js
@@ -1,31 +1,14 @@
 const express = require('express');
-const { query, param } = require('express-validator');
+const { param } = require('express-validator');
 const validatePayload = require('../../middleware/express-validator');
 const { getActivities, getActivitiesByID, getActivitiesTags } = require('../../controllers/activities');
+const paginationPayload = require('../../middleware/express-validator/pagination-payload');
 
 const activitiesRouter = express.Router();
 
 activitiesRouter.get(
   '/',
-  [
-    query('page')
-      .default(1)
-      .isNumeric()
-      .toInt()
-      .withMessage('Must be a number')
-      .isInt({ min: 1 })
-      .withMessage('Number min 1'),
-    query('limit')
-      .default(20)
-      .isNumeric()
-      .toInt()
-      .withMessage('Must be a number')
-      .isInt({ min: 1 })
-      .withMessage('Number min 1'),
-    query('q')
-      .default('')
-      .toLowerCase(),
-  ],
+  paginationPayload,
   validatePayload,
   getActivities,
 );
--- routes/tags/index.js
+++ routes/tags/index.js
@@ -1,31 +1,13 @@
 const express = require('express');
-const { query } = require('express-validator');
 const validatePayload = require('../../middleware/express-validator');
 const { getTags } = require('../../controllers/tags');
+const paginationPayload = require('../../middleware/express-validator/pagination-payload');
 
 const tagsRouter = express.Router();
 
 tagsRouter.get(
   '/',
-  [
-    query('page')
-      .default(1)
-      .isNumeric()
-      .toInt()
-      .withMessage('Must be a number')
-      .isInt({ min: 1 })
-      .withMessage('Number min 1'),
-    query('limit')
-      .default(20)
-      .isNumeric()
-      .toInt()
-      .withMessage('Must be a number')
-      .isInt({ min: 1 })
-      .withMessage('Number min 1'),
-    query('q')
-      .default('')
-      .toLowerCase(),
-  ],
+  paginationPayload,
   validatePayload,
   getTags,
 );
